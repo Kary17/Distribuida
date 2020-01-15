8e5e94d34b084a3bbe044d71ae17d7d3f769487b
Kary17 <56895104+Kary17@users.noreply.github.com>
2020-01-14 22:48:06 -0500

    eliminar

--- PD1920_DEBER09_PINEDA_VERONICA/microprofile-payara/microprofile/src/main/java/com/distribuida/rest/app/RestAplicacion.java
+++
@@ -1,9 +0,0 @@
-package com.distribuida.rest.app;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class RestAplicacion extends Application {
-
-}
